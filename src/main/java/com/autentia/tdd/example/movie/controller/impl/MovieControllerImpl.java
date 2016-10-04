250638bf9b62367029a4b40fc372cf518191c2bc
Borja Lazaro <blazaro@autentia.com>
2016-10-04 17:17:17 +0200

    Se implementa la funcionalidad que pasa a verde el test del caso de exito del controlador.

--- src/main/java/com/autentia/tdd/example/movie/controller/impl/MovieControllerImpl.java
+++ src/main/java/com/autentia/tdd/example/movie/controller/impl/MovieControllerImpl.java
@@ -1,8 +1,12 @@
 package com.autentia.tdd.example.movie.controller.impl;
 
+import java.util.List;
+
 import com.autentia.tdd.example.movie.controller.MovieController;
 import com.autentia.tdd.example.movie.controller.response.CatalogResponse;
+import com.autentia.tdd.example.movie.model.Movie;
 import com.autentia.tdd.example.movie.service.MovieCatalog;
+import com.autentia.tdd.example.movie.service.exception.MovieServiceException;
 
 public class MovieControllerImpl implements MovieController {
 
@@ -13,7 +17,18 @@ public class MovieControllerImpl implements MovieController {
 	}
 
 	public CatalogResponse search() {
-		return null;
+		try {
+			return buildCatalogResponse(movieCatalog.search());
+		} catch (MovieServiceException e) {
+			return new CatalogResponse("ERROR");
+		}
+	}
+
+	private CatalogResponse buildCatalogResponse(List<Movie> movies) {
+		if(movies != null && !movies.isEmpty()){
+			return new CatalogResponse(movies);
+		}
+		return new CatalogResponse("No hay peliculas disponibles.");
 	}
 
 }
--- src/main/java/com/autentia/tdd/example/movie/controller/response/CatalogResponse.java
+++ src/main/java/com/autentia/tdd/example/movie/controller/response/CatalogResponse.java
@@ -1,17 +1,31 @@
 package com.autentia.tdd.example.movie.controller.response;
 
+import java.util.Collections;
 import java.util.List;
 
 import com.autentia.tdd.example.movie.model.Movie;
 
 public class CatalogResponse {
 
+	private final List<Movie> movies;
+	private final String errorMessage;
+
+	public CatalogResponse(List<Movie> movies) {
+		this.movies = movies;
+		this.errorMessage= "";
+	}
+
+	public CatalogResponse(String errorMessage) {
+		this.errorMessage = errorMessage;
+		this.movies=Collections.emptyList();
+	}
+
 	public String getErroMessage() {
 		return null;
 	}
 
 	public List<Movie> getMovies() {
-		return null;
+		return movies;
 	}
 
 }
